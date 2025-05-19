89247d6ac18a75e33280ad2f4dbf8e2d21c645c8
GMiraa <guilherme.mira@sptech.school>
2025-05-19 18:48:38 -0300

    Aplicação da API WEB DATA VIZ nas páginas html

---
+++ SITE_V2/src/controllers/usuarioController.js
@@ -0,0 +1,112 @@
+var usuarioModel = require("../models/usuarioModel");
+
+function autenticar(req, res) {
+
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+
+    if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está indefinida!");
+    } else {
+
+        usuarioModel.autenticar(email, senha)
+            .then(
+                function (resultadoAutenticar) {
+                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+
+                    if (resultadoAutenticar.length == 1) {
+                        console.log(resultadoAutenticar);
+
+                        usuarioModel.buscarPerfil(resultadoAutenticar[0].idUsuario)
+                            .then((resultadoPerfil) => {
+                                if (resultadoPerfil.length > 0) {
+                                    res.json({
+                                        id: resultadoAutenticar[0].idUsuario,
+                                        email: resultadoAutenticar[0].email,
+                                        nome: resultadoAutenticar[0].nome,
+                                        cargo: resultadoAutenticar[0].cargo
+                                    });
+                                }
+                            })
+                    } else if (resultadoAutenticar.length == 0) {
+                        res.status(403).send("Email e/ou senha inválido(s)");
+                    } else {
+                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                    }
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+
+}
+
+function cadastrar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var NomeEmpresa = req.body.nomeEmpresaServer;
+    var Email = req.body.emailServer;
+    var rua = req.body.ruaServer;
+    var bairro = req.body.nomeEmpresaServer;
+    var numero = req.body.numeroServer;
+    var sigla = req.body.siglaServer;
+    var cidade = req.body.cidadeServer;
+    var cnpj = req.body.cnpjServer;
+    var codigo = req.body.codigoServer;
+
+    // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+    usuarioModel.cadastrar(codigo, NomeEmpresa, cnpj, Email, rua, bairro, cidade, numero, sigla)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao realizar o cadastro! Erro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+function cadastrarUsuario(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var NomeUsuario = req.body.nomeServer;
+    var EmailUsuario = req.body.emailServer;
+    var telefoneUsuario = req.body.telefoneServer;
+    var cargo = req.body.cargoServer;
+    var senha = req.body.senhaServer;
+    var codigo = req.body.codigoServer;
+
+    // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+    usuarioModel.cadastrarUsuario(codigo, NomeUsuario, telefoneUsuario, EmailUsuario, senha, cargo)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao realizar o cadastro! Erro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+module.exports = {
+    autenticar,
+    cadastrar,
+    cadastrarUsuario
+}
---
+++ SITE_V2/src/models/usuarioModel.js
@@ -0,0 +1,50 @@
+var database = require("../database/config")
+
+function buscarPerfil(idUsuario){
+
+    var instrucaoSql = `SELECT * FROM usuario WHERE idUsuario = ${idUsuario}`;
+  
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+  }
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+        SELECT idUsuario, nome, email, cargo FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+// Coloque os mesmos parâmetros utilizados na Controller. Vá para a var instrucaoSql
+function cadastrar(codigo, NomeEmpresa, cnpj, Email, rua, bairro, cidade, numero, sigla) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", codigo, NomeEmpresa, cnpj, Email, rua , bairro, cidade, numero, sigla);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO empresa (idEmpresa, nome, cnpj, email, rua, bairro, cidade, numero, UF) VALUES ('${codigo}','${NomeEmpresa}','${cnpj}', '${Email}', '${rua}', '${bairro}', '${cidade}', '${numero}', '${sigla}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function cadastrarUsuario(codigo, NomeUsuario, telefoneUsuario, EmailUsuario, senha, cargo) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", NomeUsuario, telefoneUsuario, EmailUsuario, senha, cargo);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO usuario (fk1Empresa, nome, telefone, email, senha, cargo) VALUES ('${codigo}', '${NomeUsuario}','${telefoneUsuario}', '${EmailUsuario}', '${senha}', '${cargo}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    autenticar,
+    buscarPerfil,
+    cadastrar,
+    cadastrarUsuario
+};
